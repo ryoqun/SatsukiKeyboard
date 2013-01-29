6c04f345517b95f96ef684a18d3f2475989600d2
Ryo Onodera <ryoqun@gmail.com>
2013-01-29 15:43:42 +0900

    Implement an initial driver, which do nothing

--- SatsukiKeyboard.cpp
+++ SatsukiKeyboard.cpp
@@ -1,1 +1,20 @@
-/* add your code here */
+#include <IOKit/IOLib.h>
+#include <IOKit/hid/IOHIDUsageTables.h>
+
+#include "SatsukiKeyboard.h"
+
+#define super IOHIDEventDriver
+
+OSDefineMetaClassAndStructors(SatsukiKeyboard, IOHIDEventDriver)
+
+bool SatsukiKeyboard::start(IOService *provider)
+{
+  printf("SatsukiKeyboard start\n");
+  return super::start(provider);
+}
+
+void SatsukiKeyboard::stop(IOService *provider)
+{
+  printf("SatsukiKeyboard stop\n");
+  return super::stop(provider);
+}
--- SatsukiKeyboard.h
+++ SatsukiKeyboard.h
@@ -1,1 +1,10 @@
-/* add your code here */
+#include <IOKit/hidevent/IOHIDEventDriver.h>
+
+class SatsukiKeyboard : public IOHIDEventDriver
+{
+  OSDeclareDefaultStructors(SatsukiKeyboard)
+
+public:
+  virtual bool start(IOService *provider);
+  virtual void stop(IOService *provider);
+};
