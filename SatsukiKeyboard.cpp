0a4887525bafe047b9c97e66f71b3fd920bf0d04
Ryo Onodera <ryoqun@gmail.com>
2013-01-29 16:15:43 +0900

    Implement almost all functionality

--- SatsukiKeyboard.cpp
+++ SatsukiKeyboard.cpp
@@ -10,13 +10,380 @@ OSDefineMetaClassAndStructors(SatsukiKeyboard, IOHIDEventDriver)
 bool SatsukiKeyboard::start(IOService *provider)
 {
   printf("SatsukiKeyboard start\n");
+  resetMode();
   return super::start(provider);
 }
 
 void SatsukiKeyboard::stop(IOService *provider)
 {
   printf("SatsukiKeyboard stop\n");
+  resetMode();
   return super::stop(provider);
+}
+
+void SatsukiKeyboard::resetMode()
+{
+  shiftPressed = false;
+  resetSpaceMode();
+  resetShiftMode();
+  resetTenkeyMode();
+}
+
+void SatsukiKeyboard::resetSpaceMode()
+{
+  spaceMode = spaceModeUsed = false;
+}
+
+void SatsukiKeyboard::resetShiftMode()
+{
+  shiftMode = false;
+}
+
+void SatsukiKeyboard::resetTenkeyMode()
+{
+  tenkeyMode = tenkeyModeUsed = false;
+}
+
+bool SatsukiKeyboard::isPressedDown(UInt32 value)
+{
+  return value == PRESS_DOWN;
+}
+
+bool SatsukiKeyboard::isPressedUp(UInt32 value)
+{
+  return value == PRESS_UP;
+}
+
+void SatsukiKeyboard::dispatchPressDown(UInt32 usage)
+{
+  dispatch(usage, PRESS_DOWN);
+}
+
+void SatsukiKeyboard::dispatchPressUp(UInt32 usage)
+{
+  dispatch(usage, PRESS_UP);
+}
+
+void SatsukiKeyboard::dispatchShiftDown()
+{
+  dispatchPressDown(kHIDUsage_KeyboardLeftShift);
+  shiftPressed = true;
+}
+
+void SatsukiKeyboard::dispatchShiftUp()
+{
+  dispatchPressUp(kHIDUsage_KeyboardLeftShift);
+  shiftPressed = false;
+}
+
+void SatsukiKeyboard::dispatch(UInt32 usage, UInt32 value, IOOptionBits options)
+{
+  super::dispatchKeyboardEvent(currentTimeStamp, currentUsagePage, usage, value, options);
+}
+
+bool SatsukiKeyboard::translateTenkeyMode(UInt32 originalUsage,
+                                          UInt32 &outUsage)
+{
+  bool translated = true;
+  UInt32 usage;
+
+  switch (originalUsage) {
+    case kHIDUsage_KeyboardQ:
+      usage = kHIDUsage_Keyboard1;
+      break;
+    case kHIDUsage_KeyboardW:
+      usage = kHIDUsage_Keyboard2;
+      break;
+    case kHIDUsage_KeyboardE:
+      usage = kHIDUsage_Keyboard3;
+      break;
+    case kHIDUsage_KeyboardR:
+      usage = kHIDUsage_Keyboard4;
+      break;
+    case kHIDUsage_KeyboardT:
+      usage = kHIDUsage_Keyboard5;
+      break;
+    case kHIDUsage_KeyboardY:
+      usage = kHIDUsage_Keyboard6;
+      break;
+    case kHIDUsage_KeyboardU:
+      usage = kHIDUsage_Keyboard7;
+      break;
+    case kHIDUsage_KeyboardI:
+      usage = kHIDUsage_Keyboard8;
+      break;
+    case kHIDUsage_KeyboardO:
+      usage = kHIDUsage_Keyboard9;
+      break;
+    case kHIDUsage_KeyboardP:
+      usage = kHIDUsage_Keyboard0;
+      break;
+
+    case kHIDUsage_KeyboardA:
+      usage = kHIDUsage_KeyboardTab;
+      break;
+    case kHIDUsage_KeyboardS:
+      usage = kHIDUsage_KeyboardDeleteForward;
+      break;
+    case kHIDUsage_KeyboardD:
+      usage = kHIDUsage_KeyboardDeleteOrBackspace;
+      break;
+    case kHIDUsage_KeyboardF:
+      usage = kHIDUsage_KeyboardEscape;
+      break;
+
+    case kHIDUsage_KeyboardH:
+      usage = kHIDUsage_KeyboardLeftArrow;
+      break;
+    case kHIDUsage_KeyboardJ:
+      usage = kHIDUsage_KeyboardDownArrow;
+      break;
+    case kHIDUsage_KeyboardK:
+      usage = kHIDUsage_KeyboardUpArrow;
+      break;
+    case kHIDUsage_KeyboardL:
+      usage = kHIDUsage_KeyboardRightArrow;
+      break;
+    case kHIDUsage_KeyboardSemicolon:
+      usage = kHIDUsage_KeyboardReturnOrEnter;
+      break;
+    default:
+      translated = false;
+      break;
+  }
+
+  if (translated) {
+    outUsage = usage;
+  }
+
+  return translated;
+}
+
+bool SatsukiKeyboard::translateSpaceMode(UInt32 originalUsage,
+                                         UInt32 &outUsage,
+                                         bool &outShiftModifier)
+{
+  bool translated = true;
+  UInt32 usage;
+  bool shiftModifier;
+
+  switch (originalUsage) {
+    case kHIDUsage_KeyboardQ:
+      usage = kHIDUsage_Keyboard1;
+      shiftModifier = true;
+      break;
+    case kHIDUsage_KeyboardW:
+      usage = kHIDUsage_Keyboard2;
+      shiftModifier = true;
+      break;
+    case kHIDUsage_KeyboardE:
+      usage = kHIDUsage_Keyboard3;
+      shiftModifier = true;
+      break;
+    case kHIDUsage_KeyboardR:
+      usage = kHIDUsage_Keyboard4;
+      shiftModifier = true;
+      break;
+    case kHIDUsage_KeyboardT:
+      usage = kHIDUsage_Keyboard5;
+      shiftModifier = true;
+      break;
+    case kHIDUsage_KeyboardY:
+      usage = kHIDUsage_Keyboard6;
+      shiftModifier = true;
+      break;
+    case kHIDUsage_KeyboardU:
+      usage = kHIDUsage_Keyboard7;
+      shiftModifier = true;
+      break;
+    case kHIDUsage_KeyboardI:
+      usage = kHIDUsage_Keyboard8;
+      shiftModifier = true;
+      break;
+    case kHIDUsage_KeyboardO:
+      usage = kHIDUsage_Keyboard9;
+      shiftModifier = true;
+      break;
+    case kHIDUsage_KeyboardP:
+      usage = kHIDUsage_Keyboard0;
+      shiftModifier = true;
+      break;
+
+    case kHIDUsage_KeyboardA:
+      usage = kHIDUsage_KeyboardHyphen;
+      shiftModifier = true;
+      break;
+    case kHIDUsage_KeyboardS:
+      usage = kHIDUsage_KeyboardHyphen;
+      shiftModifier = false;
+      break;
+    case kHIDUsage_KeyboardD:
+      usage = kHIDUsage_KeyboardOpenBracket;
+      shiftModifier = false;
+      break;
+    case kHIDUsage_KeyboardF:
+      usage = kHIDUsage_KeyboardCloseBracket;
+      shiftModifier = false;
+      break;
+    case kHIDUsage_KeyboardG:
+      usage = kHIDUsage_KeyboardQuote;
+      shiftModifier = false;
+      break;
+    case kHIDUsage_KeyboardH:
+      usage = kHIDUsage_KeyboardQuote;
+      shiftModifier = true;
+      break;
+    case kHIDUsage_KeyboardJ:
+      usage = kHIDUsage_KeyboardOpenBracket;
+      shiftModifier = true;
+      break;
+    case kHIDUsage_KeyboardK:
+      usage = kHIDUsage_KeyboardCloseBracket;
+      shiftModifier = true;
+      break;
+    case kHIDUsage_KeyboardL:
+      usage = kHIDUsage_KeyboardGraveAccentAndTilde;
+      shiftModifier = true;
+      break;
+    case kHIDUsage_KeyboardSemicolon:
+      usage = kHIDUsage_KeyboardSemicolon;
+      shiftModifier = true;
+      break;
+
+    case kHIDUsage_KeyboardX:
+      usage = kHIDUsage_KeyboardGraveAccentAndTilde;
+      shiftModifier = false;
+      break;
+    case kHIDUsage_KeyboardC:
+      usage = kHIDUsage_KeyboardBackslash;
+      shiftModifier = false;
+      break;
+    case kHIDUsage_KeyboardV:
+      usage = kHIDUsage_KeyboardEqualSign;
+      shiftModifier = false;
+      break;
+    case kHIDUsage_KeyboardB:
+      usage = kHIDUsage_KeyboardBackslash;
+      shiftModifier = true;
+      break;
+    case kHIDUsage_KeyboardN:
+      usage = kHIDUsage_KeyboardEqualSign;
+      shiftModifier = true;
+      break;
+    case kHIDUsage_KeyboardM:
+      usage = kHIDUsage_KeyboardSpacebar;
+      shiftModifier = true;
+      break;
+    case kHIDUsage_KeyboardComma:
+      usage = kHIDUsage_KeyboardComma;
+      shiftModifier = true;
+      break;
+    case kHIDUsage_KeyboardPeriod:
+      usage = kHIDUsage_KeyboardPeriod;
+      shiftModifier = true;
+      break;
+    case kHIDUsage_KeyboardSlash:
+      usage = kHIDUsage_KeyboardSlash;
+      shiftModifier = true;
+      break;
+    default:
+      translated = false;
+      break;
+  }
+
+  if (translated) {
+    outUsage = usage;
+    outShiftModifier = shiftModifier;
+  }
+
+  return translated;
+}
+
+void SatsukiKeyboard::handleKeyboardMode(UInt32 usage,
+                                         UInt32 value,
+                                         IOOptionBits options)
+{
+  bool ignore = false;
+
+  printf("Key event: %u %u %u\n",
+         (unsigned int) usage,
+         (unsigned int)value,
+         (unsigned int)options);
+
+  if (usage == kHIDUsage_KeyboardSpacebar) {
+    if (isPressedDown(value)) {
+      ignore = true;
+      spaceMode = true;
+    } else if (isPressedUp(value)) {
+      if (spaceModeUsed) {
+        ignore = true;
+        if (shiftPressed && !shiftMode) {
+          dispatchShiftUp();
+        }
+      } else {
+        dispatchPressDown(kHIDUsage_KeyboardSpacebar);
+      }
+      resetSpaceMode();
+    } else {
+      printf("unrecognized key event value: %u\n", (unsigned int)value);
+    }
+  } else if (usage == kHIDUsage_KeyboardHenkan) {
+    ignore = true;
+
+    if (isPressedDown(value) and !shiftPressed) {
+      shiftMode = true;
+      dispatchShiftDown();
+    } else if (isPressedUp(value) and shiftPressed) {
+      shiftMode = false;
+      dispatchShiftUp();
+    }
+  } else if (usage == kHIDUsage_KeyboardMuhenkan) {
+    ignore = true;
+
+    if (isPressedDown(value)) {
+      tenkeyMode = true;
+    } else {
+      resetTenkeyMode();
+    }
+  }
+
+  if (ignore) {
+    return;
+  }
+
+  UInt32 originalUsage = usage;
+  bool shiftModifier = false;
+
+  if (spaceMode) {
+    if (isPressedDown(value)) {
+      spaceModeUsed = true;
+    }
+
+    if (spaceModeUsed and translateSpaceMode(originalUsage, usage, shiftModifier)) {
+      if (shiftModifier and isPressedDown(value) and !shiftPressed) {
+        dispatchShiftDown();
+      } else if (!shiftModifier and isPressedDown(value) and shiftPressed) {
+        dispatchShiftUp();
+      }
+    }
+  }
+
+  if (tenkeyMode) {
+    if (isPressedDown(value)) {
+      tenkeyModeUsed = true;
+    }
+    if (tenkeyModeUsed) {
+      translateTenkeyMode(originalUsage, usage);
+    }
+  }
+
+  dispatch(usage, value, options);
+
+  if (shiftModifier and shiftPressed and isPressedUp(value) and !shiftMode) {
+    dispatchShiftUp();
+  } else if (!shiftModifier and !shiftPressed and isPressedUp(value) and shiftMode) {
+    dispatchShiftDown();
+  }
 }
 
 void SatsukiKeyboard::dispatchKeyboardEvent(AbsoluteTime timeStamp,
@@ -25,10 +392,16 @@ void SatsukiKeyboard::dispatchKeyboardEvent(AbsoluteTime timeStamp,
                                             UInt32 value,
                                             IOOptionBits options)
 {
-  printf("Keyboard Event\n");
-  super::dispatchKeyboardEvent(timeStamp,
-                               usagePage,
-                               usage,
-                               value,
-                               options);
+  if (usagePage != kHIDPage_KeyboardOrKeypad) {
+    super::dispatchKeyboardEvent(timeStamp,
+                                 usagePage,
+                                 usage,
+                                 value,
+                                 options);
+    return;
+  }
+
+  currentTimeStamp = timeStamp;
+  currentUsagePage = usagePage;
+  handleKeyboardMode(usage, value, options);
 }
--- SatsukiKeyboard.h
+++ SatsukiKeyboard.h
@@ -14,4 +14,45 @@ public:
                                      UInt32 usage,
                                      UInt32 value,
                                      IOOptionBits options=0);
+
+private:
+  static const UInt32 PRESS_DOWN = 1;
+  static const UInt32 PRESS_UP = 0;
+  static const UInt32 kHIDUsage_KeyboardHenkan = 138;
+  static const UInt32 kHIDUsage_KeyboardMuhenkan = 139;
+  bool spaceMode, spaceModeUsed;
+  bool shiftMode;
+  bool tenkeyMode, tenkeyModeUsed;
+  bool shiftPressed;
+
+  AbsoluteTime currentTimeStamp;
+  UInt32 currentUsagePage;
+
+  bool isPressedDown(UInt32 value);
+  bool isPressedUp(UInt32 value);
+
+  void dispatchPressDown(UInt32 usage);
+  void dispatchPressUp(UInt32 usage);
+
+  void dispatchShiftDown();
+  void dispatchShiftUp();
+
+  void dispatch(UInt32 usage,
+                UInt32 value,
+                IOOptionBits options=0);
+
+  void handleKeyboardMode(UInt32 usage,
+                          UInt32 value,
+                          IOOptionBits options);
+
+  bool translateSpaceMode(UInt32 original_usage,
+                          UInt32 &usage,
+                          bool &shift_modifier);
+  bool translateTenkeyMode(UInt32 original_usage,
+                           UInt32 &usage);
+
+  void resetMode();
+  void resetSpaceMode();
+  void resetShiftMode();
+  void resetTenkeyMode();
 };
